d1bc3a0d3e94da179178e694ea1b58746957b5f9
Ftnt <jairo.rocano@gmail.com>
2021-11-19 21:40:50 -0500

    add ependence Express

--- clase006/index.js
+++
@@ -1,5 +0,0 @@
-const colors = require('colors');
-
-let text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quia similique consequatur cupiditate, neque esse maiores numquam illum atque est!'
-console.log(text.rainbow);
-
